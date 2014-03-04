17ed9addd5abc9cd604764f7e46e5f8dfb6b99fe
Jean-Dominique Nguele <jd@lodgeo.com>
2014-03-04 13:34:03 +0000

    first commit B

---
+++ main.c
@@ -0,0 +1,18 @@
+
+#include “me.h”
+#include “awesome.h”
+
+int main (int argc, char** argv)
+{
+    while (true)
+    {
+        sleep();
+        eat();
+        win();
+    }
+    
+    return 0;
+}
+
+
+
